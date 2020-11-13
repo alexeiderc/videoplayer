0b03d7e3be4e863846c0cde32ded8fde0f3b8dfa
Alexeider Castillo Coello <accd.solver@gmail.com>
2020-11-13 10:31:39 -0800

    Making jQuery Plugin for video

--- video.js
+++ video.js
@@ -1,12 +1,48 @@
 /*
  * ACCD Plugin video
- * 
+ * Varios metodos para trabajar los videos
 */
 
-var 
-    VideoPlugin = ({
-             
-    })
-
-
+(function($) {
     
+    // realizar acciones al estar llegar un tiempo determinado
+    $.fn.video = function(opts) {
+        
+        var 
+            status = [],
+            mode,
+            check,
+            notified = false;
+            
+        options = $.extend({}, $.fn.video.defaultOptions, opts)
+        
+        this.each(function() {
+            var 
+                v  = $(this), // element
+                vd = v.duration,
+               
+            if (opts.handleEnoughPercent && opts.onEnoughPercent) {
+                
+                opts.onEnoughPercent()
+            }
+            
+            // -- commons
+            
+            
+        })
+    }
+    
+    // default options
+    $.fn.video.defaultOptions = {
+        handleEnoughTime: false,
+        enoughTime: 0.0,
+        handleEnoughPercent: false,
+        enoughPercent: 0,
+        onPlaying: null,
+        onPause: null,
+        onSeeking: null,
+        onEnoughTime: null,
+        onEnoughPercent: null
+    }
+    
+})(jQuery)
